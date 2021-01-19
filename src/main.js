81f3ac39424b3efb38361461b8a62b820717d71f
bling33wu <bling33wu@163.com>
2021-01-19 10:22:04 +0800

    genggai

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = "http://192.168.1.3:8888/"
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
